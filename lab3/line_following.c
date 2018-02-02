68d8a3cd62fddbc1822a22a2c257195cbd6ee60d
Oliver Zhang <ozz@andrew.cmu.edu>
2018-02-01 22:41:12 -0500

    Added PID controller to line follower

--- lab3/line_following.c
+++ lab3/line_following.c
@@ -5,11 +5,19 @@
 
 * Feel free to modify any part of these codes.
 **********************************************/
+//Global PID Variables
+float last_error;
+
 //Set parameters
-const int THRESHOLD = 15;
-const int MOTOR_POWER = 10;
-const int UPDATE_INTERVAL = 5;
-const float CURVE_RATE = 0.5;
+const int LOW_THRESHOLD = 36;
+const int HIGH_THRESHOLD = 66;
+//Brightness value at the "gray zone" between line and background
+const int THRESHOLD = (LOW_THRESHOLD + HIGH_THRESHOLD)/2;
+const int MOTOR_POWER = 50;	 //Base motor power to control the speed
+const int UPDATE_INTERVAL = 1; //Delay updates by x miliseconds
+
+const float kP = 2;
+const float kD = 20;
 
 /*****************************************
 * Main function - Needs changing
@@ -18,41 +26,46 @@ const float CURVE_RATE = 0.5;
 
 task main()
 {
-	int motorPowerL;
-	int motorPowerR;
-
 	//Wait for sensor initialization
 	wait1Msec(50);
 
 	// Find the line by turning right
 	while(SensorValue[S3] >= THRESHOLD){
-		motorPowerL = MOTOR_POWER;
-		motorPowerR = -1 * MOTOR_POWER;
+		motor[motorA] = MOTOR_POWER;
+		motor[motorB] = -1 * MOTOR_POWER;
 
 		nxtDisplayCenteredTextLine(2, "Searching: %d", SensorValue[S3]);
 		wait1Msec(UPDATE_INTERVAL);
 	}
+	// Start line following
+	clearTimer(T1);
 	while(1){
-		// Curve left away from the line
-		if(SensorValue[S3] < THRESHOLD){
-			motorPowerL = MOTOR_POWER * CURVE_RATE;
-			motorPowerR = MOTOR_POWER;
-
-			nxtDisplayCenteredTextLine(2, "Turn Left");
+		int dt = time1[T1];
+		clearTimer(T1);
+		if(dt == 0) {
+			continue;
 		}
-		else{
-			motorPowerL = MOTOR_POWER;
-			motorPowerR = MOTOR_POWER * CURVE_RATE;
+		float actual = SensorValue[S3];
+		float error = THRESHOLD - actual; // Negative if too dark, positive if too bright
+		float d_error = (error-last_error)/dt;
+		last_error = error;
 
-			nxtDisplayCenteredTextLine(2, "Turn Right");
-		}
+		float signal = kP * error + kD * d_error;
+		signal = signal/15;
 
-		nxtDisplayCenteredTextLine(3, "Light: %d", SensorValue[S3]);
+
+		nxtDisplayCenteredTextLine(2, "Brightness: %d", SensorValue[S3]);
+		nxtDisplayCenteredTextLine(3, "Signal: %f", signal);
 
 		// Update motors
-		motor[motorA] = motorPowerL;
-		motor[motorB] = motorPowerR;
-
+		if(signal > 0){ //Turn right
+			motor[motorA] = MOTOR_POWER * (-2*signal*signal+1); 	//Right Motor
+			motor[motorB] = MOTOR_POWER; //Left Motor
+		}
+		else { //Turn left
+			motor[motorA] = MOTOR_POWER; 	//Right Motor
+			motor[motorB] = MOTOR_POWER * (-2*signal*signal+1); //Left Motor
+		}
 		wait1Msec(UPDATE_INTERVAL);
 	}
 }
