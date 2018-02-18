00449ca5e6333409fe5809fba3f52952581bc5c5
Dimitris Konstantinidis <dimgkonst@gmail.com>
2018-02-18 13:28:23 -0500

    Added Padding Function
    It requires that we input the radius size in terms of pixels

---
+++ lab5/create_CSpace.m
@@ -0,0 +1,27 @@
+function [CSpace] = create_CSpace(Map,radius)
+%radius is passed in as pixel length
+pad = 2;
+CSpace = uint8(Map);
+%go through the space once, padding the edges of the map
+for i = 1:(size(CSpace,1))
+    for j = 1:(size(CSpace,2))
+        if (i<radius)|| (i>(size(CSpace,1) - radius)) || (j<radius) || (j>(size(CSpace,2) - radius))
+            CSpace(i,j) = pad;
+        end
+    end
+end
+[xgrid, ygrid] = meshgrid(1:size(CSpace,2), 1:size(CSpace,1));
+%go through it once more and pad the obstacles in the map
+for i = (1+radius):(size(CSpace,1) - radius)
+    for j = (1+radius):(size(CSpace,2) - radius)
+        if (CSpace(i,j) == 1)
+            %window = CSpace(i-radius:i+radius,j-radius:j+radius);
+            mask = ((xgrid-j).^2 + (ygrid-i).^2) <= radius.^2;
+            window = CSpace(mask);
+            indices = window == 0;
+            window(indices) = pad;
+            CSpace(mask) = window;
+        end
+    end
+end
+end
