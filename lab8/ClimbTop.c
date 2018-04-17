b1ab0d7050660beac1b4dde08067cfab5609e195
olimengia <mengyunx@andrew.cmu.edu>
2018-04-16 20:32:49 -0400

    lab 8 demo ready

--- lab8/ClimbTop.c
+++ lab8/ClimbTop.c
@@ -21,35 +21,6 @@ task main()
 
 		nxtDisplayCenteredTextLine(2, "Rung: %d", floor(nMotorEncoder[motorC]/180));
 		int rung = floor(nMotorEncoder[MotorC]/180);
-		if(!sensed && SensorValue[S2] >= 30 && floor(nMotorEncoder[motorC]/180)%2 == 0)
-		{
-			playSound(soundBeepBeep);
-			nxtDisplayCenteredTextLine(3, "Even: %f", SensorValue[S2]);
-			sensed = True
-
-			//finalCount = nMotorEncoder[motorC] + 0.4*rotationCount;
-			finalCount = rung * 180 + 45;
-		}
-		else if(!sensed && SensorValue[S2] >= 40)
-		{
-			playSound(soundBeepBeep);
-			nxtDisplayCenteredTextLine(3, "Odd: %f", SensorValue[S2]);
-			sensed = True
-			//finalCount = nMotorEncoder[motorC] + 0.20*rotationCount;
-			finalCount = rung * 180 + 40;
-		}
-		while(sensed)
-		{
-				nxtDisplayCenteredTextLine(4, "Sensor: %f", SensorValue[S2]);
-				nxtDisplayCenteredTextLine(5, "Counts: %f", nMotorEncoder[motorC]);
-				writeDebugStreamLine("%d", SensorValue[S2]);
-				if(nMotorEncoder[motorC] <= finalCount){
-					motor[motorC] = 50;
-				}
-				else{
-					motor[motorC] = 0;
-				}
-		}
 	}
 	motor[motorC] = 0;
 	return;
