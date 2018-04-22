00f666f2ef9afefdb804ddda095f8df7300004b6
Dimitris Konstantinidis <dimgkonst@gmail.com>
2018-04-22 18:56:56 -0400

    Small changes on the PID componenets of the arm and the IK function

--- lab9/Idontcare.m
+++ lab9/Idontcare.m
@@ -1,11 +1,11 @@
-x = 2;
-y = 4;
+x = -3;
+y = 3;
 l1 = 3.75;
 l2 = 2.5;
 Beta = acos((x^2 + y^2 - l1^2 - l2^2)/(2*l1*l2));
 s1 = (y*(l1 + l2*cos(Beta)) - x*l2*sin(Beta))/(x^2+y^2);
 c1 = (x+s1*(l2*sin(Beta)))/(l1+l2*cos(Beta));
 
-Theta1 = atan2(s1,c1);
+Theta1 = 180*atan2(s1,c1)/pi;
 
-Theta2 = Beta + Theta1;
+Theta2 = 180*Beta/pi + Theta1;
