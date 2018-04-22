7015e25601f3e42b2cf2a255a443f4130923b994
olimengia <mengyunx@andrew.cmu.edu>
2018-04-22 18:30:38 -0400

    absolute c space

--- lab9/generate_C_space.m
+++ lab9/generate_C_space.m
@@ -6,14 +6,17 @@ l2 = 2.5; % unit is inch
 c_space_matrix = zeros(361,181); % Resolution is 1 degree
 
 t1 = 0:180;
-t2 = -180:180;
+t2 = 0:360;
 
 c_space = ones(181*361,2)*200;  % First col: theta1, second col: theta2. Multiply 200 is for later thresholding
 
 [t1,t2] = meshgrid(t1, t2);
 
-x = cosd(t1)*l1 + cosd(t1+t2)*l2;
-y = sind(t1)*l1 + sind(t1+t2)*l2;
+
+% x = cosd(t1)*l1 + cosd(t1+t2)*l2;
+% y = sind(t1)*l1 + sind(t1+t2)*l2;
+x = cosd(t1)*l1 + cosd(t2)*l2;
+y = sind(t1)*l1 + sind(t2)*l2;
 x1 = cosd(t1)*l1;
 y1 = sind(t1)*l1;
 
@@ -43,14 +46,14 @@ for row = 1:size(x,1)
         end
     end
 end
-% figure(1);
-% c_space = c_space(1:counter-1,:);
-% scatter(c_space(:,1), c_space(:,2), 5,'filled')
-% 
-% figure(2);
-% x = cosd(c_space(:,1))*l1 + cosd(c_space(:,2) + c_space(:,1))*l2;
-% y = sind(c_space(:,1))*l1 + sind(c_space(:,2) + c_space(:,1))*l2;
-% scatter(x,y,'.');
-% axis([-8,8,-8,8])
-c_space_matrix_flipped = flipud(c_space_matrix);
-save('c_space_matrix.mat','c_space_matrix_flipped');
+figure(1);
+c_space = c_space(1:counter-1,:);
+scatter(c_space(:,1), c_space(:,2), 5,'filled')
+
+figure(2);
+x = cosd(c_space(:,1))*l1 + cosd(c_space(:,2))*l2;
+y = sind(c_space(:,1))*l1 + sind(c_space(:,2))*l2;
+scatter(x,y,'.');
+axis([-8,8,-8,8])
+c_space_matrix_absolute = flipud(c_space_matrix);
+save('c_space_matrix_absolute.mat','c_space_matrix_absolute');
